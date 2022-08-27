7822786b15ebb06663dfdb97617dab20822f7ab1
Rachel Holmquist <rachelflorek18@outlook.com>
2022-08-27 11:34:09 -0700

    characters validated

--- password.js
+++ password.js
@@ -1,19 +1,45 @@
-let password = "Evergreen73";
+const e = require("cors");
+
+let password = "Evergreen54";
 
 console.log(`${password} ${password.length}`);
 
-let lowerLetters = /[a-z]/g;
-let upperLetters = /[A-Z]/g;
-let numeric = /[0-9]/g;
 
-if(password.length < 10){
-    console.log("password must be at least 10 characters");
+let characterValidate = /([a-zA-Z0-9!@#$%^&*])/;
+
+if(password.match(characterValidate)){
+    console.log("Password characters are valid");
+} else{
+    console.log("password characters are not valid");
+    }
+
+let length = (password.length < 10);
+let maxLength = (password.length > 20);
+
+if(`${length}` === true){
+    console.log("Password must contain at least 10 characters");
 }
-if(password.length > 20){
-    console.log("password must be less than 20 characters");
+if(`${maxLength}` === true){
+    console.log("Password can not exceed 20 characters.")
 }
-if (password.match(lowerLetters) && password.match(upperLetters) && password.match(numeric)){
-    console.log("password is valid");
-} else {
-    console.log("password is invalid");
-}
+
+// if(checkLowerLetters && upperLetters && numeric){
+//     console.log("All characters needed for a succesful password.")
+// } else {
+//     console.log("no");
+// }
+
+// if (password.match(lowerLetters)){
+// } else {
+//     console.log("password needs lower case letters.")
+// }
+// if (password.match(upperLetters)){
+// }  else {
+//     console.log("password needs Upper case letters.");
+// }
+// if (password.match(numeric)){
+    
+// } else {
+//     console.log("password needs a numeric value.");
+// } 
+
