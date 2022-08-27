24fa942653a59213c3ea42785e69d273d62eed8d
Rachel Holmquist <rachelflorek18@outlook.com>
2022-08-27 11:38:39 -0700

    password length validated

--- password.js
+++ password.js
@@ -16,30 +16,10 @@ if(password.match(characterValidate)){
 let length = (password.length < 10);
 let maxLength = (password.length > 20);
 
-if(`${length}` === true){
-    console.log("Password must contain at least 10 characters");
-}
-if(`${maxLength}` === true){
-    console.log("Password can not exceed 20 characters.")
+if(`${length}` === true || `${maxLength}` === true){
+    console.log("Password must be between 10 and 20 characters.");
+} else {
+    console.log("password has correct amount of characters.");
 }
 
-// if(checkLowerLetters && upperLetters && numeric){
-//     console.log("All characters needed for a succesful password.")
-// } else {
-//     console.log("no");
-// }
-
-// if (password.match(lowerLetters)){
-// } else {
-//     console.log("password needs lower case letters.")
-// }
-// if (password.match(upperLetters)){
-// }  else {
-//     console.log("password needs Upper case letters.");
-// }
-// if (password.match(numeric)){
-    
-// } else {
-//     console.log("password needs a numeric value.");
-// } 
 
